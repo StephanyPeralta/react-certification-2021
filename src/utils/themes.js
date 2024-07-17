5314b8563510330e10d14d8ccc5f238ef473472e
Stephany Peralta <73197053+StephanyPeralta@users.noreply.github.com>
2024-07-17 17:13:36 -0600

    Update themes.js

--- src/utils/themes.js
+++ src/utils/themes.js
@@ -1,10 +1,10 @@
 const Themes = {
   light: {
-    primary: '#80cbc4',
-    secondary: '#ecfcf8',
+    primary: '#ae90c8',
+    secondary: '#F8E8FF',
     background: '#fff',
     card: '#fff',
-    button: '#ae90c8',
+    button: '#FEC69E',
     color: '#000',
     input: '#e0e0e0',
     hover: 'rgba(0, 0, 0, 0.1)',
@@ -14,7 +14,7 @@ const Themes = {
     secondary: '#594853',
     background: '#44303c',
     card: '#594853',
-    button: '#eab595',
+    button: '#F3E09B',
     color: '#fff',
     input: '#fafafa',
     hover: 'rgba(255, 255, 255, 0.2)',
